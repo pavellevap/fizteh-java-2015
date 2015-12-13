d0595832f69047afe73ed7c47f8a0757819e8004
Pavel <pavelgramovich@mail.ru>
2015-12-13 03:27:08 +0300

    Counter added

--- pavellevap/src/main/java/ru/fizteh/fivt/students/pavellevap/threads/Counter.java
+++ pavellevap/src/main/java/ru/fizteh/fivt/students/pavellevap/threads/Counter.java
@@ -1,7 +1,57 @@
 package ru.fizteh.fivt.students.pavellevap.threads;
 
 public class Counter {
+
+    static public class Runner {
+        volatile int currNumber = 1;
+
+        public class NumberPrinter implements Runnable {
+            NumberPrinter(int number) {
+                this.number = number;
+            }
+
+            int number;
+
+            @Override
+            public void run() {
+                while (true) {
+                    synchronized (Runner.this) {
+                        try {
+                            while (currNumber != number)
+                                Runner.this.wait();
+                            System.out.println("Thread-" + number);
+                                    Thread.sleep(1000);
+                            currNumber++;
+                            Runner.this.notifyAll();
+                        } catch (InterruptedException ex) {}
+                    }
+                }
+            }
+        }
+
+        void run(int amountOfThreads) {
+            for (int i = 0; i < amountOfThreads; i++)
+                (new Thread(new NumberPrinter(i + 1))).start();
+
+            while (true) {
+                synchronized (this) {
+                    try {
+                        while (currNumber != amountOfThreads + 1)
+                            this.wait();
+                        currNumber = 1;
+                        this.notifyAll();
+                    } catch (InterruptedException ex) {}
+                }
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int n = 0;
+        if (args.length > 0)
+            n = Integer.parseInt(args[0]);
+
+        (new Runner()).run(n);
     }
 }
